c0f0075fb8dfcfe9c765c98828c8dcb71bc9273a
Senioriousg <senioriousg@gmail.com>
2022-11-18 17:29:25 +0800

    version 1.1

--- Mathjax-to-plain.user.js
+++ Mathjax-to-plain.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         MathJax to plain
 // @namespace    https://github.com/Senioriousg
-// @version      1.0
+// @version      1.1
 // @description  Convert MathJax2 equation to plain
 // @author       Seniorious
 // @match        *://*/*
-// @updateURL    https://github.com/Senioriousg/MathJax-to-plain/raw/main/Mathjax-to-plain.user.js
-// @downloadURL  https://github.com/Senioriousg/MathJax-to-plain/raw/main/Mathjax-to-plain.user.js
+// @updateURL    https://github.com/Senioriousg/MathJax-to-plain/raw/main/Mathjax-to-plain.js
+// @downloadURL  https://github.com/Senioriousg/MathJax-to-plain/raw/main/Mathjax-to-plain.js
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
@@ -20,16 +20,12 @@ window.MJ2toPlain = function() {
     return pattern.test(ele.id) && ele.type == "math/tex";
   }).map(function() {
     $(this).after("$" + $(this).html() + "$");
+    $(this).remove();
   });
-  $("script").filter(function(index, ele) {
-    return pattern.test(ele.id) && ele.type == "math/tex";
-  }).remove();
   $("script").filter(function(index, ele) {
     return pattern.test(ele.id) && ele.type == "math/tex; mode=display";
   }).map(function() {
     $(this).after("$$" + $(this).html() + "$$");
+    $(this).remove();
   });
-  $("script").filter(function(index, ele) {
-    return pattern.test(ele.id) && ele.type == "math/tex; mode=display";
-  }).remove();
 };
